4c7ea0c98be463a7620714285380ec7d237a1883
viikranta <vicky.scorpions@gmail.com>
2018-05-01 18:30:11 -0400

    initial commit
    initial commit

---
+++ spring-boot-microservice-forex-service/src/main/java/com/in28minutes/springboot/microservice/example/forex/springbootmicroserviceforexservice/ExchangeValueRepository.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.microservice.example.forex.springbootmicroserviceforexservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ExchangeValueRepository extends 
+
+    JpaRepository<ExchangeValue, Long>{
+
+  ExchangeValue findByFromAndTo(String from, String to);
+
+}
